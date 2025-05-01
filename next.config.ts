a945c295794b7a2f85f7e2f4f074f78e569bd345
Electron <yogesh.dubey.0804@gmail.com>
2025-05-02 00:46:26 +0530

    feat: add TypeScript configuration to ignore build errors

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
   images: {
     domains: ["ui.aceternity.com"], // Allow images from this domain
   },
+  typescript:{
+    ignoreBuildErrors: true, // Ignore TypeScript errors during build
+  },
   reactStrictMode: true,
 };
 
